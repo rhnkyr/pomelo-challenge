2d61ad08b1b3db99cdbbfb8f1d78193f6ce27085
Erhan Kayar <rhnkyr@gmail.com>
2020-07-17 21:35:49 +0700

    initial

---
+++ .history/index_20200717211957.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/json-arranger',
+        handler: (request, h) => {
+
+            const payload = request.payload;
+
+            //Holds flat objects    
+            let obj = []
+
+            for (const key in payload) {
+
+                const objects = payload[key]// Get objects
+
+                for (const o in objects) {
+
+                    obj.push(objects[o])// create flat json object array
+
+                }
+
+            }
+
+            const nested = obj.filter((elt, idx, arr) => {
+                const parent = arr.find(e => e.id === elt.parent_id); // check the object is parent by element
+                if (!parent) return true;
+                (parent.children = parent.children || []).push(elt);
+            });
+
+            return nested
+
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
